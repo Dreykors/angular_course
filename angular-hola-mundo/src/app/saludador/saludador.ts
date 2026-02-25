3de122a8970b6ebeb6b4f6e70e1a3493a5db829a
Elam Murillo <elammurillo01@gmail.com>
2026-02-25 17:29:24 -0600

    Primera App

---
+++ angular-hola-mundo/src/app/saludador/saludador.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-saludador',
+  standalone: true,
+  imports: [],
+  templateUrl: './saludador.html',
+  styleUrl: './saludador.css',
+})
+export class Saludador {
+
+}
